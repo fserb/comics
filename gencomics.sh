ec788b6f558a6e1617830e5823139991d90aa137
Fernando Serboncini <fserb@fserb.com.br>
2007-12-13 01:11:39 -0200

    arghhh

--- gencomics.sh
+++ gencomics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH="/home/fserb/run/lib:"
-LD_RUN_PATH="/home/fserb/run/lib:"
-PATH="$PATH:/home/fserb/run/bin"
-PYTHONPATH="/home/fserb/.python"
+export LD_LIBRARY_PATH="/home/fserb/run/lib:"
+export LD_RUN_PATH="/home/fserb/run/lib:"
+export PATH="$PATH:/home/fserb/run/bin"
+export PYTHONPATH="/home/fserb/.python"
 ./gencomics.py
